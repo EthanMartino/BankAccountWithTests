8ca38aa9049033b6dae294d9d6f68e49c089ec1a
Ethan Martino <49508204+ET7026@users.noreply.github.com>
2020-02-05 14:50:30 -0800

    Add project files.

---
+++ BankAccountWithTests.Tests/BankAccountTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountWithTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountWithTests.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void Deposit_PositiveValue_AddsToBalance()
+        {
+            // Arrange - Get Objects/Variables set up
+            BankAccount acc = new BankAccount("123456");
+            double depositAmount = 100.00;
+            double initialBalance = 0.00;
+            double expectedBalance = 100.00;
+
+            // Act - Call the method under test (Deposit() in this case)
+            acc.Deposit(depositAmount);
+
+            // Assert - Check for appropriate results
+            Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+    }
+}
---
+++ BankAccountWithTests/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountWithTests
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+
+        public BankAccount(string accountNum)
+        {
+            AccountNumber = accountNum;
+        }
+
+        public string AccountNumber 
+        { 
+            get 
+            { 
+                return accountNumber; 
+            } 
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Account Number cannot be null or whitespace.");
+                }
+                accountNumber = value; 
+            }
+        }
+
+        public string Owner { get; private set; }
+
+        public double Balance { get; private set; }
+
+        /// <summary>
+        /// Deposits a positive amount of money into the account and returns the new balance
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when amount is 0 or less</exception>
+        /// <param name="amount">The amount of money to deposit</param>
+        public double Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, "You must deposit a positive amount.");
+            }
+            Balance += amount;
+            return Balance;
+        }
+
+        public double Withdrawl()
+        {
+
+
+            return Balance;
+        }
+    }
+}
